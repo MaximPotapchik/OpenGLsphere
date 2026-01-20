d6560bd115da7646d7e09a998f2a84ef236a8f80
MaximPotapchik <maximp1012@gmail.com>
2026-01-20 10:46:25 -0500

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+int main(){
+	if (!glfwInit()) 
+	{
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My GLFW Window", NULL, NULL);
+
+	while (!glfwWindowShouldClose(window)) {
+
+   
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window); 
+        glfwPollEvents(); 
+    }
+
+	return 0;
+}
